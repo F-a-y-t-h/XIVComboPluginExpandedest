a49f9351a436cd0004b56dda0483871de0c724dc
F-a-y-t-h <14098907+F-a-y-t-h@users.noreply.github.com>
2023-01-11 07:26:36 +0000

    a

--- XIVComboExpanded/Attributes/BlueInactiveAttribute.cs
+++ XIVComboExpanded/Attributes/BlueInactiveAttribute.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿/*
+
+using System;
 using System.Collections.Generic;
 using XIVComboExpandedestPlugin.Services;
 
@@ -26,3 +28,4 @@ namespace XIVComboExpandedestPlugin.Attributes
         internal List<uint> Actions { get; set; } = new();
     }
 }
+*/
